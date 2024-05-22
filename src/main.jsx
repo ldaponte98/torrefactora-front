77f838acda0130d4727d706bd5e0180991be67da
ldaponte981 <ldaponte98@gmail.com>
2024-05-22 04:52:57 -0500

    Ajuste readme

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <App />
     </LocalizationProvider>
-    
   </React.StrictMode>,
 )
